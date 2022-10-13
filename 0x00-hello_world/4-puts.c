9b3814e78b74c0be61e3f69c858a36c06ae1b2ec
Mikelonia <onajitemike4real@gmail.com>
2022-10-13 15:20:14 -0700

    4*

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*Now to the main code
- * main- serves as the entry point for the program */
-int main (void)
+/*Now to the main code main- serves as the entry point for the program
+  */
+int main(void)
 {puts ("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
