9e431a8518787ca6e865817652628bf9181f6616
Mikelonia <onajitemike4real@gmail.com>
2022-10-13 15:41:55 -0700

    5*

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return:0 if no errors, return non zero if errors
  * */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n";
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 		}
