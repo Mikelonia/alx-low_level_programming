c26875d9cf2e2cb6aecb7d8f5fc643f6d6546498
Mikelonia <onajitemike4real@gmail.com>
2022-10-13 15:32:58 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*Now to the main code main- serves as the entry point for the program
-/* code function also does the same */
+* code function also does the same */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{ puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
