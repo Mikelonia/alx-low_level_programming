026848532be5635ae38c919d358e688fabbbf1ce
Mikelonia <onajitemike4real@gmail.com>
2022-10-16 12:14:43 -0700

    task 3 correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,20 @@
 /**
  * main-program entry point.
  * Return:0 if no error, non zero if error.
- **/
+*/
 int main(void)
-{	char i;
+{
+	char i;
 	char j;
-	for(i='a'; i<='z'; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
-	for(j='A';j<='Z'; j++)
+	for ( j = 'A'; j <='Z'; j++)
 	{
-		putchar(j);
+		putchar (j);
 	}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
