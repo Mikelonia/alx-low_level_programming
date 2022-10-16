aaf38116e5fd7adbda881b9ab4fc17ab66dd8b2b
Mikelonia <onajitemike4real@gmail.com>
2022-10-16 10:40:19 -0700

    5*

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,12 @@
 /**
  * main-program entry point
  * Return: 0 if no error, non zero if error
- * 
- */
+*/
 int main(void)
 {
 	char i ;
 
-	for (i = '0' ; i <= '9' ; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar (i);
 	}
