00645670612596ef2bc62733712ff58e4701df9b
Mikelonia <onajitemike4real@gmail.com>
2022-10-19 05:14:02 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/* main - program entry point
+ * Return : 0 if succeed, non zero if fail
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+		return (0);
+}
+
