ec1a4656d6f33391d55226d704f179ffdb91a601
Mikelonia <onajitemike4real@gmail.com>
2022-10-27 00:28:05 -0700

    add main.c

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,16 +1,11 @@
 #include "main.h"
 /**
- * main - check the code
  * Return: Always 0.
- */
+ * main - check the code
+*/
 
-void print_most_numbers(void)
+int main(void)
 {
-	char c;
-	
-	for (c = '0'; c <= '9'; c++)
-		if (!(c == '2' || c == '4'))
-			_putchar (c);
-}
-_putchar ('\n');
+	print_most_numbers();
+	return (0);
 }
