092192a023809111826802a6ea772ced54f4e280
Mikelonia <onajitemike4real@gmail.com>
2022-10-30 13:52:22 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,16 @@
-#include<stdio.h>
+#include"main.h"
 /**
- * main- program entry stars here
+ * main- program entry starts here
  * Return 0 if success, non zero if fail
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-return (0);
+	char la, lb;
+
+	for (la = '0'; la <= '9'; la++)
+	{
+		for (lb = 'a'; lb <= 'z'; lb++)
+			_putchar ('\n');
+	}
+	_putchar ('\n');
 }
