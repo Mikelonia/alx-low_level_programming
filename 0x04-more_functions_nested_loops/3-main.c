aa25c10782d256c6ebb14926475b45628fcbc0ae
Mikelonia <onajitemike4real@gmail.com>
2022-10-30 13:03:55 -0700

    modification

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -1,17 +1,10 @@
 #include "main.h"
-
 /**
- * main - check the code
- * Return: Always 0.
- * print_numbers - print numbers 0123456789\n
- */
-void print_numbers(void)
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
 {
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar (c);
-	}
-_putchar ('\n');
+	print_numbers();
+	return (0);
 }
