15ae99d9c8612c4f00ff07924d9bfe26f1382f02
Mikelonia <onajitemike4real@gmail.com>
2022-10-30 14:06:25 -0700

    addf

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include"main.h"
 /**
- * main- program entry starts here
- * Return 0 if success, non zero if fail
+ * print_alphabet - program entry starts here
+ * Return: 0 if success, non zero if fail
+ * _putchar: adds the characters
  */
 void print_alphabet_x10(void)
 {
-	char la, lb;
+	char la;
+	int ten;
 
-	for (la = '0'; la <= '9'; la++)
-	
-		for (lb = 'a'; lb <= 'z'; lb++)
-			_putchar (lb);
-	
+	for (ten = 0; ten <= 9; ten++)
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar (la);
 	_putchar ('\n');
 }
