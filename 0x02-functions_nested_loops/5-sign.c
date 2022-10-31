9a25bcb5685a06236ea4e361638d98636614a672
Mikelonia <onajitemike4real@gmail.com>
2022-10-31 11:33:26 -0700

    Task 5, all thanks to Jehovah

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,27 @@
-#include<stdio.h>
-
+#include"main.h"
 /**
- *
- * main - program entry starts here
- *
- * *Return: 0 if success, non zero if error
- *
- * */
-
-int main(void)
-
+ * print_sign - prints the sign of a number
+ * @n: the number of the sign to be printed
+ * Return: 1 if number is greater than zero,
+ * 0 if number is zero
+ * -1 if number is less than zero
+ */
+int print_sign(int n)
 {
-
-	return (0);
-
+	if (n > '0')
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == '0')
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
 }
+
