9f9dd9941eb9e588e8f195374f5192cc2b7e31cd
Mikelonia <onajitemike4real@gmail.com>
2022-12-08 11:09:44 -0800

    Modification of 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/*Now to the main code main- serves as the entry point for the program
-* code function also does the same */
+/**
+ * main-serves as the entry point for the program
+ * Return: 0 if no error
+ */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
