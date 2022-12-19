91a02c748c6dc598f5177ebe004cfd05cd493c2d
Mikelonia <onajitemike4real@gmail.com>
2022-12-19 12:38:17 -0800

    Task 4

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *  _strlen - Returns the length of a string.
- *  @str: string.
+ *  @s: string.
  *   Return: length.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t len = 0;
+	int len = 0;
 
-	while (*str++)
+	while (*s++)
 		len++;
 	return (len);
 }
