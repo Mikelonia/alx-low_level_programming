6d0f745d73cf6939380f687b1416be91c54921f5
Mikelonia <onajitemike4real@gmail.com>
2023-05-04 10:31:25 -0700

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Printing a hash table.
+ * @ht: A pointer pointing to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned char comma_flag = 0;
+	unsigned long int m;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (m = 0; m < ht->size; m++)
+	{
+		if (ht->array[m] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[m];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
+}
